40a05d6748371a3320929092a7cdb9ed578e9fc8
Luanda Vital <luavital0@gmail.com>
2023-10-15 00:12:46 -0300

    aula 01 - estrutura e início da construção do back

---
+++ src/test/java/br/com/luanda/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luanda.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
